d27f89653f6dd13f17f93e936a4a725f09e6a2a2
Siqi Lu <lu.siqi@outlook.com>
2015-01-27 16:10:29 +0800

    Release 0.4.2

--- gruntfile.js
+++ gruntfile.js
@@ -1,64 +1,75 @@
 ﻿module.exports = function(grunt) {
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower: grunt.file.readJSON('bower.json'),
+
         clean: ["css/", "dist/"],
+
         jshint: {
-            dev: ['Gruntfile.js']
+            grunt: ['Gruntfile.js']
         },
+
         lesslint: {
             options: {
                 csslint: {
                     "font-faces": false
                 }
             },
-            dev: {
+            core: {
                 src: ['less/*.less']
             },
         },
+
         less: {
             options: {
-                isCompat: true
+                isCompat: true,
+                strictMath: true,
+                sourceMap: true,
+                outputSourceFiles: true
             },
-            dev: {
+            core: {
                 files: {
                     "css/fonts.css": "less/fonts.less",
                     "css/fonts-ie.css": "less/fonts-ie.less"
                 }
             }
         },
+
         autoprefixer: {
             options: {
                 browsers: ['> 1%', '> 1% in CN', 'last 2 versions', 'ie 8', 'ie 9']
             },
-            dev: {
-                src: ['css/*.css']
+            core: {
+                src: ['css/**/*.css', '!css/**/*.min.css']
             }
         },
+
         cssmin: {
             options: {
                 advance: true,
                 compatibility: 'ie8',
                 keepSpecialComments: 0
-                    //root:
             },
-            dev: {
+            core: {
                 files: [{
                     expand: true,
                     cwd: './',
-                    src: ['css/*.css'],
-                    dest: 'dist/',
+                    src: ['css/**/*.css', '!css/**/*.min.css'],
+                    dest: './',
                     ext: '.min.css'
                 }]
             }
         },
+
         copy: {
             options: {
                 timestamp: true
             },
-            dev: {
+            core: {
                 files: [{
                     expand: true,
-                    src: ['css/*.css'],
+                    src: ['css/**'],
                     dest: 'dist/',
                     filter: 'isFile'
                 }, {
@@ -67,8 +78,24 @@
                     dest: 'dist/'
                 }],
             },
-        }
+        },
 
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release %VERSION%',
+                commitFiles: ['-a'],
+                createTag: false,
+                tagName: '%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false,
+                pushTo: 'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
@@ -78,6 +105,7 @@
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-bump');
 
     grunt.registerTask('default', ['clean', 'jshint', 'lesslint', 'less', 'autoprefixer', 'cssmin', 'copy']);
 };
