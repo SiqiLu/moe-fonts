48b0f64e03cb808023233427eab0ded9efc397da
Siqi Lu <lu.siqi@outlook.com>
2015-02-25 23:46:20 +0800

    Release 0.4.5

--- gruntfile.js
+++ gruntfile.js
@@ -1,112 +1,110 @@
-﻿module.exports = function(grunt) {
+﻿module.exports = function (grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        bower: grunt.file.readJSON('bower.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower: grunt.file.readJSON('bower.json'),
 
-        clean: ["css/", "dist/"],
+    clean: ["css/", "dist/"],
 
-        jshint: {
-            grunt: ['Gruntfile.js']
-        },
+    jshint: {
+      grunt: ['Gruntfile.js']
+    },
 
-        lesslint: {
-            options: {
-                csslint: {
-                    "font-faces": false
-                }
-            },
-            core: {
-                src: ['less/*.less']
-            },
-        },
-
-        less: {
-            options: {
-                isCompat: true,
-                strictMath: true,
-                sourceMap: true,
-                outputSourceFiles: true
-            },
-            core: {
-                files: {
-                    "css/fonts.css": "less/fonts.less",
-                    "css/fonts-ie.css": "less/fonts-ie.less"
-                }
-            }
-        },
-
-        autoprefixer: {
-            options: {
-                browsers: ['> 1%', '> 1% in CN', 'last 2 versions', 'ie 8', 'ie 9']
-            },
-            core: {
-                src: ['css/**/*.css', '!css/**/*.min.css']
-            }
-        },
-
-        cssmin: {
-            options: {
-                advance: true,
-                compatibility: 'ie8',
-                keepSpecialComments: 0
-            },
-            core: {
-                files: [{
-                    expand: true,
-                    cwd: './',
-                    src: ['css/**/*.css', '!css/**/*.min.css'],
-                    dest: './',
-                    ext: '.min.css'
-                }]
-            }
-        },
-
-        copy: {
-            options: {
-                timestamp: true
-            },
-            core: {
-                files: [{
-                    expand: true,
-                    src: ['css/**'],
-                    dest: 'dist/',
-                    filter: 'isFile'
-                }, {
-                    expand: true,
-                    src: ['fonts/**'],
-                    dest: 'dist/'
-                }],
-            },
-        },
-
-        bump: {
-            options: {
-                files: ['package.json', 'bower.json'],
-                updateConfigs: [],
-                commit: true,
-                commitMessage: 'Release %VERSION%',
-                commitFiles: ['-a'],
-                createTag: true,
-                tagName: '%VERSION%',
-                tagMessage: 'Version %VERSION%',
-                push: true,
-                pushTo: 'origin master',
-                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-                globalReplace: false
-            }
+    lesslint: {
+      options: {
+        csslint: {
+          "font-faces": false
         }
-    });
+      },
+      core: {
+        src: ['less/*.less']
+      },
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-lesslint');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-bump');
+    less: {
+      options: {
+        isCompat: true,
+        strictMath: true
+      },
+      core: {
+        files: {
+          "css/fonts.css": "less/fonts.less",
+          "css/fonts-ie.css": "less/fonts-ie.less"
+        }
+      }
+    },
 
-    grunt.registerTask('default', ['clean', 'jshint', 'lesslint', 'less', 'autoprefixer', 'cssmin', 'copy']);
-    grunt.registerTask('release', ['bump']);
+    autoprefixer: {
+      options: {
+        browsers: ['> 1%', '> 1% in CN', 'last 2 versions', 'ie 8', 'ie 9']
+      },
+      core: {
+        src: ['css/**/*.css', '!css/**/*.min.css']
+      }
+    },
+
+    cssmin: {
+      options: {
+        advance: true,
+        compatibility: 'ie8',
+        keepSpecialComments: 0
+      },
+      core: {
+        files: [{
+          expand: true,
+          cwd: './',
+          src: ['css/**/*.css', '!css/**/*.min.css'],
+          dest: './',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    copy: {
+      options: {
+        timestamp: true
+      },
+      core: {
+        files: [{
+          expand: true,
+          src: ['css/**'],
+          dest: 'dist/',
+          filter: 'isFile'
+        }, {
+          expand: true,
+          src: ['fonts/**'],
+          dest: 'dist/'
+        }],
+      },
+    },
+
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release %VERSION%',
+        commitFiles: ['-a'],
+        createTag: true,
+        tagName: '%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin master',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-lesslint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.registerTask('default', ['clean', 'jshint', 'lesslint', 'less', 'autoprefixer', 'cssmin', 'copy']);
+  grunt.registerTask('release', ['default', 'bump']);
 };
